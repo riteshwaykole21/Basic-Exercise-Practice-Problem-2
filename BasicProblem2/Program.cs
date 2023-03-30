140b17a2ab159099d002dfba3cf86097b6c8aa0d
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-03-30 21:22:33 +0530

    [Ritesh]add : Print The Age Taken input From User

---
+++ BasicProblem2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblem2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Takes an Age");
+            Console.WriteLine("Enter a Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    Age age = new Age();
+                    age.AgeExa();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
